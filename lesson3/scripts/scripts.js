826224dc73bb890fa87daeed89758021e3c222a1
jhmower <jhmower@gmail.com>
2022-09-30 22:10:46 -0600

    Rough Draft for Chamber of commerce web plan

---
+++ lesson3/scripts/scripts.js
@@ -0,0 +1,11 @@
+lastmodified = document.querySelector("#last-updated");
+lastmodified.textContent = new Date(document.lastModified);
+// let lastModified = document.lastModified;
+// document.getElementById("#last-updated").textContent = lastModified;
+
+let d = new Date();
+let year = d.getFullYear();
+// console.log(year)
+// document.getElementById("#copywriteYear").textContent = year;
+let copywrite = document.querySelector("#copywriteYear");
+copywrite.insertAdjacentText("beforeend", year)
