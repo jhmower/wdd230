aa2a20eac81549377c639ad9f3702344e7dddaaf
jhmower <jhmower@gmail.com>
2022-10-08 11:34:32 -0600

    First draft of home screen chamber

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,28 @@
+lastmodified = document.querySelector("#last-updated");
+lastmodified.textContent = new Date(document.lastModified);
+// let lastModified = document.lastModified;
+// document.getElementById("#last-updated").textContent = lastModified;
+
+let d = new Date();
+let year = d.getFullYear();
+// console.log(year)
+// document.getElementById("#copywriteYear").textContent = year;
+let copywrite = document.querySelector("#copywriteYear");
+copywrite.insertAdjacentText("beforeend", year)
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open")
+    document.getElementById("hamburgerBtn").classList.toggle("open")
+}
+
+const x = document.getElementById("hamburgerBtn")
+
+x.onclick = toggleMenu
+
+
+const datefield = document.querySelector("#currentDate");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+datefield.innerHTML = `<em>${fulldate}</em>`;
