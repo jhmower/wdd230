10ac1a49613471211052d16475921c109be4d0e0
jhmower <jhmower@gmail.com>
2022-10-29 12:01:05 -0600

    2nd draft of week 7 assignment

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,3 +1,4 @@
+// picture loading
 let imagesToLoad = document.querySelectorAll("img[data-src]");
 const loadImages = (image) => {
   image.setAttribute("src", image.getAttribute("data-src"));
@@ -5,7 +6,6 @@ const loadImages = (image) => {
     image.removeAttribute("data-src");
   };
 };
-
 
 if ("IntersectionObserver" in window) {
 const observer = new IntersectionObserver((items, observer) => {
@@ -24,3 +24,21 @@ imagesToLoad.forEach((img) => {
     loadImages(img);
 });
 }
+
+
+
+// last visit
+const visitDisplay = document.querySelector(".last-visit");
+const today = Date.now();
+
+let lastVisit = Number(window.localStorage.getItem("last-visit"));
+let dateLastVisit = new Date(lastVisit)
+
+if (lastVisit !== null) {
+	visitDisplay.textContent = dateLastVisit;
+} else {
+	visitDisplay.textContent = `This is your first visit!`;
+}
+
+localStorage.setItem("last-visit", today);
+
