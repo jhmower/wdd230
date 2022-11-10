4ac12e5266e89fd257161869a850355bc5ff3e18
jhmower <jhmower@gmail.com>
2022-11-09 21:38:02 -0700

    fixed windchill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -6,7 +6,8 @@ let windSpeed = windSpeedHTML.textContent;
 
 if (temp <= 50 && windSpeed >3.0) {
     f = 35.74 + (0.6215*temp) - (35.75 * (windSpeed ** 0.16)) + (0.4275*temp*(windSpeed ** 0.16))
-    windchillHTML.textContent = f.toFixed(1)
+    windchillDisplay = f.toFixed(1)
+    windchillHTML.textContent = `Windchill: ${windchillDisplay}`
 } else {
     windchillHTML.textContent = "Windchill: N/A"
 }
