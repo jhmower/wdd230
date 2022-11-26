e0c5242b8dccb9a2c2dc0058ab962c65532b6747
jhmower <jhmower@gmail.com>
2022-11-26 12:53:34 -0700

    rough draft contact page, auto spotlight home

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -31,3 +31,61 @@ if (d.getDay() == 1 || d.getDay() == 2) {
     upperHeader.style.display = "block"
 }
 // console.log(d.getDay())
+
+
+
+
+
+
+// // Home page spotlight
+
+// const requestURL = 'scripts/data.json';
+// const cards = document.querySelector('spotlights');
+
+// function displayMembers(member) {
+//     // Create elements to add to the document
+//     let card = document.createElement('section');
+//     let h2 = document.createElement('h2');
+//     let logo = document.createElement('img');
+//     let address = document.createElement('p');
+//     let phone = document.createElement('p');
+//     let url = document.createElement('a');
+
+
+  
+//     // Change the textContent property of the h2 element to contain the prophet's full name
+//     h2.textContent = `${member.name}`;
+//     address.textContent = `${member.address}`;
+//     phone.textContent = `${member.phone}`;
+//     url.textContent = `${member.url}`;
+//     url.setAttribute('href', member.url);
+
+  
+//     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+//     logo.setAttribute('src', member.logo);
+//     logo.setAttribute('alt', `Logo of ${member.name}`);
+//     logo.setAttribute('loading', 'lazy');
+  
+//     // Add/append the section(card) with the h2 element
+//     card.setAttribute('class', "directory-section")
+//     card.appendChild(h2);
+//     card.appendChild(logo);
+//     card.appendChild(address);
+//     card.appendChild(phone);
+//     card.appendChild(url);
+  
+//     // Add/append the existing HTML div with the cards class with the section(card)
+//     document.querySelector('div#directory').appendChild(card);
+// }
+
+// fetch(requestURL)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (jsonObject) {
+//     const members = jsonObject['members'];
+//     console.table(members)
+//     const member1 = jasonObject['members[0]']
+//     console.table(member1)
+//     members.forEach(displayMembers);
+// });
--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,23 +1,22 @@
 const requestURL = 'scripts/data.json';
 const cards = document.querySelector('directory');
+let spotlightNumber = 1;
 
 function displayMembers(member) {
     // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
+    let card = document.createElement('div');
+    let title = document.createElement('p');
     let logo = document.createElement('img');
     let address = document.createElement('p');
     let phone = document.createElement('p');
-    let url = document.createElement('a');
+    let hr = document.createElement('hr');
 
 
-  
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${member.name}`;
+    title.textContent = `${member.name}`;
     address.textContent = `${member.address}`;
     phone.textContent = `${member.phone}`;
-    url.textContent = `${member.url}`;
-    url.setAttribute('href', member.url);
+
 
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
@@ -26,15 +25,30 @@ function displayMembers(member) {
     logo.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
-    card.setAttribute('class', "directory-section")
-    card.appendChild(h2);
+    if (spotlightNumber == 1) {
+      card.setAttribute('id', "spotlight-1");
+      spotlightNumber = 2;
+    } else if (spotlightNumber == 2 ) {
+      card.setAttribute('id', "spotlight-2");
+      spotlightNumber = 3;
+    } else if (spotlightNumber == 3 ) {
+      card.setAttribute('id', "spotlight-3");
+      spotlightNumber = 4;
+    } else {
+      card.setAttribute('id', "spotlight-4");
+      spotlightNumber = 4;
+    }
+
+    card.appendChild(title);
     card.appendChild(logo);
+    card.appendChild(hr);
     card.appendChild(address);
     card.appendChild(phone);
-    card.appendChild(url);
+    
+    // card.appendChild(url);
   
     // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div#directory').appendChild(card);
+    document.querySelector('div#spotlights').appendChild(card);
 }
 
 function findGold(member) {
@@ -42,6 +56,24 @@ function findGold(member) {
     gold_members.push(member)
   };
 
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
 }
 
 fetch(requestURL)
@@ -53,32 +85,10 @@ fetch(requestURL)
     console.table(members)
     // const member1 = jasonObject['members.']
     // console.table(member1)
-    members.forEach(displayMembers);
     gold_members = []
     members.forEach(findGold)
+    shuffle(gold_members)
     console.log(gold_members)
+    gold_members.forEach(displayMembers);
 
 });
-
-
-
-// grid/list switching
-
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("#directory");
-
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
-
-gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("directory-grid");
-	display.classList.remove("directory-list");
-});
-
-listbutton.addEventListener("click", showList); // example using defined function
-
-function showList() {
-	display.classList.add("directory-list");
-	display.classList.remove("directory-grid");
-}
